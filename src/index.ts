5df6f1c12e2235f55af51f0a1fa30cd75305565f
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-04-02 17:20:41 +0530

    Handle promise errors
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,37 @@
 import core from "@actions/core";
-import { Repository } from 'nodegit';
-import { parseContent } from "./utils";
-import { createTask } from "./asana";
-import { getAsanaUserId } from "./utils/asana_users_utils";
+import {Repository} from 'nodegit';
+import {parseContent} from "./utils/utils";
+import {createTask} from "./asana";
+import {getAsanaUserId} from "./utils/asana_users_utils";
 
 async function run() {
   try {
     core.info("Init...");
-    
+
     const asanaToken = core.getInput("asana-token");
-    const rawProjectIds = core.getInput("project-id");
+    const rawProjectIds = core.getInput("project-ids");
     const projectIds = rawProjectIds ? JSON.parse(rawProjectIds) : [];
     const followersIds: string[] = [];
 
-    Repository.open("../../StudioProjects/fa_flutter_mt")
-      .then(async function (repo) {
-        const from = await repo.getCommit('848816c7a5370b161b66680db1a0a075ca5da214');
-        const diffs = await from.getDiff();
-        diffs.forEach(value => {
-          value.patches().then(patches => {
-            patches.forEach(patch => {
-              patch.hunks().then(hunks => {
-                hunks.forEach(hunk => {
-                  hunk.lines().then(lines => {
-                    lines.forEach(line => {
-                      console.log(line.content());
-                      parseContent(line.content(), (username: string, task: string) => {
-                        const userId = getAsanaUserId(username);
-                        createTask(userId, task, asanaToken, projectIds, followersIds);
-                      });
-                    })
-                  });
-                });
-              });
-            });
-          });
-        })
-      });
+    const repo = await Repository.open("../../StudioProjects/fa_flutter_mt")
 
+    const from = await repo.getCommit('848816c7a5370b161b66680db1a0a075ca5da214');
+    const diffs = await from.getDiff();
+    for (const value of diffs) {
+      const patches = await value.patches();
+      for (const patch of patches) {
+        const hunks = await patch.hunks();
+        for (const hunk of hunks) {
+          const lines = await hunk.lines();
+          for (const line of lines) {
+            parseContent(line.content(), async (username: string, task: string) => {
+              const userId = getAsanaUserId(username);
+              await createTask(userId, task, asanaToken, projectIds, followersIds);
+            });
+          }
+        }
+      }
+    }
 
     // const nameToGreet = core.getInput('who-to-greet');
     // console.log(`Hello ${ nameToGreet }!`);
