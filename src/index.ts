ce6af6550d3317685f0a4c6df6bb4564a193834f
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-04-06 14:30:49 +0530

    Fetch repo
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,8 @@ async function run() {
 
     const repo = await Repository.open("./");
 
+    await repo.fetchAll();
+
     const commit = await repo.getHeadCommit();
     const diffs = await commit.getDiff();
 
