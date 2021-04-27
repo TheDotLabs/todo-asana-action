6bfd7f39e02028c3037bcf49e71a9e59be951bff
Ayush P Gupta <ayushpguptaapg@gmail.com>
2021-04-27 15:55:08 +0530

    Fix regex to extract todo message
    Signed-off-by: Ayush P Gupta <ayushpguptaapg@gmail.com>

--- dist/utils/utils.js
+++ dist/utils/utils.js
@@ -4,7 +4,7 @@ exports.parseContent = void 0;
 function parseContent(content, createTask) {
     var _a, _b, _c, _d;
     var match;
-    var regExp = /TODO\((?<username>\w+)\): (?<task>\w+)/g;
+    var regExp = /TODO\((?<username>\w+)\): (?<task>.*)/g;
     while ((match = regExp.exec(content)) !== null) {
         var username = (_b = (_a = match.groups) === null || _a === void 0 ? void 0 : _a.username) !== null && _b !== void 0 ? _b : "";
         var task = (_d = (_c = match.groups) === null || _c === void 0 ? void 0 : _c.task) !== null && _d !== void 0 ? _d : "TBD";
